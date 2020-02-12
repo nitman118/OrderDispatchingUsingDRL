c1b9c50ebb86945c18a5ea99e578ef78819f29bb
Singh <n.singh1@tue.nl>
2020-02-12 17:40:16 +0100

    Changed Sim Model, working python Script

--- AnyLogic Simulation Model/AL Simulation Data Generation/DataGeneration_BIC_PRJ2/spatial_data.py
+++ AnyLogic Simulation Model/AL Simulation Data Generation/DataGeneration_BIC_PRJ2/spatial_data.py
@@ -7,7 +7,21 @@ print(type(spatial_data))
 
 spatial_data = [line.split(',') for line in spatial_data]
 '''
-spatial_data = pd.read_csv('tripFile.txt', header=None)
+spatial_data = pd.read_csv('tripFile.txt')
 print(spatial_data.head())
+print(f"Average Reward:{spatial_data['REWARD'].mean()}")
 
+
+print(spatial_data[spatial_data['TENANT_ID']==0]['REWARD'])
+'''
+plt.plot(spatial_data['ST_AGV_START_X'],spatial_data['ST_AGV_START_Y'], label = spatial_data['TENANT_ID'])
+plt.legend()
+plt.show()
+'''
+plt.figure('Spatial Distribution of order arrivals')
+for i in range(5):
+    plt.plot(spatial_data[spatial_data['TENANT_ID']==i]['INFO_TASK_START_X'],spatial_data[spatial_data['TENANT_ID']==i]['INFO_TASK_START_Y'], label = f'Tenant {i}', marker = 'o', linestyle = '')
+plt.legend(loc = 'best', bbox_to_anchor=(1, 0.5))
+plt.title('Spatial Distribution of order arrivals')
+plt.show()
 
